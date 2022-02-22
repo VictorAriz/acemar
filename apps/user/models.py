25e7716473fd1bcd63c324b521bad2d5f80da0d9
VictorAriz <victorarizag07@gmail.com>
2022-02-22 18:45:31 -0500

    version 3

--- apps/user/models.py
+++ apps/user/models.py
@@ -5,9 +5,16 @@ from django.db import models
 class User(models.Model):
     name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    mail = models.EmailField()
-    user_name = models.CharField(max_length=50)
     document = models.PositiveSmallIntegerField()
+    phone = models.PositiveSmallIntegerField(null=True)
+    mail = models.EmailField()
+    address = models.CharField(max_length=50, null=True)
+    city = models.CharField(max_length=60, null=True)
+    state_province = models.CharField(max_length=30, null=True)
+    country = models.CharField(max_length=50, null=True)
+    website = models.URLField(null=True)
+    user_name = models.CharField(max_length=50)
+
 
 
     def __str__(self):
--- apps/user/views.py
+++ apps/user/views.py
@@ -14,12 +14,19 @@ def home(request):
 def registrarUser(request):
     name = request.POST['txtName']
     last_name= request.POST['txtLast_name']
-    mail = request.POST['txtMail']
-    user_name = request.POST['txtUser_name']
     document = request.POST['numDocument']
+    phone = request.POST['numPhone']
+    mail = request.POST['txtMail']
+    address = request.POST['txtAddress']
+    city = request.POST['txtCity']
+    state_province = request.POST['txtState_province']
+    country = request.POST['txtCountry']
+    website = request.POST['txtWebsite']
+    user_name = request.POST['txtUser_name']
+
 
     user = User.objects.create(
-        name=name, last_name=last_name, mail=mail, user_name=user_name, document=document)
+        name=name, last_name=last_name, document=document, phone=phone, mail=mail, address=address, city=city, state_province=state_province, country=country, website=website, user_name=user_name)
     messages.success(request, '¡Usuario registrado!')
     return redirect('/home')
 
@@ -34,16 +41,29 @@ def editarUser(request, id):
     # codigo = request.POST['txtCodigo']
     name = request.POST['txtName']
     last_name= request.POST['txtLast_name']
-    mail = request.POST['txtMail']
-    user_name = request.POST['txtUser_name']
     document = request.POST['numDocument']
+    phone = request.POST['numPhone']
+    mail = request.POST['txtMail']
+    address = request.POST['txtAddress']
+    city = request.POST['txtCity']
+    state_province = request.POST['txtState_province']
+    country = request.POST['txtCountry']
+    website = request.POST['txtWebsite']
+    user_name = request.POST['txtUser_name']
 
     user = User.objects.get(id=id)
     user.name = name
     user.last_name = last_name
-    user.mail = mail
-    user.user_name = user_name
     user.document = document
+    user.phone = phone
+    user.mail = mail
+    user.address = address
+    user.city = city
+    user.state_province = state_province
+    user.country = country
+    user.website = website
+    user.user_name = user_name
+
     user.save()
 
     messages.success(request, '¡Usuario actualizado!')
